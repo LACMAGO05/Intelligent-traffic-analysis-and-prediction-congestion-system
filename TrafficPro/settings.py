328c0f65905535875054589725017dd67330ff29
LACMAGO05 <rebeccalacmago@outlook.com>
2026-05-08 00:58:48 +0100

    Reintroduce SMTP settings for SendGrid: set EMAIL_HOST, EMAIL_PORT, and EMAIL_USE_TLS

--- TrafficPro/settings.py
+++ TrafficPro/settings.py
@@ -149,6 +149,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_PASSWORD = os.getenv("SENDGRID_API_KEY")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 EMAIL_TIMEOUT = 20
+EMAIL_HOST = "smtp.sendgrid.net"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 
 
 DEBUG = False
